17975029504f693b4703d64f4e47f15ca9138db4
Aiden Stern <aiden.stern@gmail.com>
2021-07-01 17:14:23 -0400

    [add] header styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,45 +1,59 @@
 import React from 'react';
 
+import Image from 'next/image';
 import Link from 'next/link';
 
+import profilePic from 'public/assets/images/nextjs-starter-banner.png';
 import { AppConfig } from 'src/utils/AppConfig';
 
 function TopBar() {
   return (
-    <div className="border-b border-gray-300">
-      <div className="pt-16 pb-8">
-        <div className="font-bold text-3xl text-gray-900">
-          {AppConfig.title}
+    <div className="p-8 relative max-w-screen-xl xs:p-24">
+      <div className="block mb-6 md:flex">
+        <div className="w-full" style={{ maxWidth: '150px' }}>
+          <div className="rounded-full transform transition-all duration-150 hover:scale-105">
+            <Image
+              className="rounded-full"
+              width="150"
+              height="150"
+              src={profilePic}
+            />
+          </div>
         </div>
-        <div className="text-xl">{AppConfig.description}</div>
-      </div>
-      <div>
-        <ul className="flex flex-wrap text-xl">
-          <li className="mr-6">
-            <Link href="/">
-              <a className="text-gray-700 border-none hover:text-gray-900">
-                Home
-              </a>
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link href="/about/">
-              <a className="text-gray-700 border-none hover:text-gray-900">
-                About
-              </a>
-            </Link>
-          </li>
-          <li className="mr-6">
-            <a
-              className="text-gray-700 border-none hover:text-gray-900"
-              href="https://github.com/aidenstern/aidenstern.dev"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </li>
-        </ul>
+        <div className="flex-none pt-6 md:pt-1 md:flex-1 md:pl-20">
+          <div className="text-5xl text-gray-900 font-bold leading-tight hover:text-black">
+            {AppConfig.title}
+          </div>
+          <div className="text-gray-600">{AppConfig.description}</div>
+          <div>
+            <ul className="mt-6 uppercase tracking-wider">
+              <li className="inline list-none pr-4">
+                <Link href="/">
+                  <a className="text-gray-700 border-none hover:text-gray-900">
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li className="inline list-none pr-4">
+                <Link href="/about/">
+                  <a className="text-gray-700 border-none hover:text-gray-900">
+                    About
+                  </a>
+                </Link>
+              </li>
+              <li className="inline list-none pr-4">
+                <a
+                  className="text-gray-700 border-none hover:text-gray-900"
+                  href="https://github.com/aidenstern/aidenstern.dev"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
