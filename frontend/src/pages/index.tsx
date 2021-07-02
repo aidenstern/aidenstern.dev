29e6c4f245f02c35e6f1bd9cfcd8d9690074cd3a
Aiden Stern <aiden.stern@gmail.com>
2021-07-02 13:16:54 -0400

    [add] placeholder experience, projects, skills sections

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -27,6 +27,57 @@ const Index = () => {
           </div>
         </div>
       </Section>
+      <Section>
+        <div className="pb-6 md:w-full md:max-w-xs md:p-0">
+          <h2 className="font-xs font-light tracking-widest text-sm text-gray-600 leading-normal uppercase">
+            Experience
+          </h2>
+        </div>
+        <div className="flex-none text-lg text-gray-600 font-light bg-white md:flex-1 md:pl-20">
+          <div className="mb-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              debitis possimus sunt, perferendis, optio voluptates id odit, quis
+              dolorem omnis veniam! Sint, nostrum perspiciatis inventore facere
+              dicta atque provident? Minus.
+            </p>
+          </div>
+        </div>
+      </Section>
+      <Section>
+        <div className="pb-6 md:w-full md:max-w-xs md:p-0">
+          <h2 className="font-xs font-light tracking-widest text-sm text-gray-600 leading-normal uppercase">
+            Projects
+          </h2>
+        </div>
+        <div className="flex-none text-lg text-gray-600 font-light bg-white md:flex-1 md:pl-20">
+          <div className="mb-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              debitis possimus sunt, perferendis, optio voluptates id odit, quis
+              dolorem omnis veniam! Sint, nostrum perspiciatis inventore facere
+              dicta atque provident? Minus.
+            </p>
+          </div>
+        </div>
+      </Section>
+      <Section>
+        <div className="pb-6 md:w-full md:max-w-xs md:p-0">
+          <h2 className="font-xs font-light tracking-widest text-sm text-gray-600 leading-normal uppercase">
+            Skills
+          </h2>
+        </div>
+        <div className="flex-none text-lg text-gray-600 font-light bg-white md:flex-1 md:pl-20">
+          <div className="mb-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              debitis possimus sunt, perferendis, optio voluptates id odit, quis
+              dolorem omnis veniam! Sint, nostrum perspiciatis inventore facere
+              dicta atque provident? Minus.
+            </p>
+          </div>
+        </div>
+      </Section>
     </Main>
   );
 };
