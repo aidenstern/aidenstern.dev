5d7547497ed8e3ae3ff10c7bfd540d9685e9e3af
Aiden Stern <aiden.stern@gmail.com>
2021-07-05 13:08:25 -0400

    [add] code block syntax highlighting

--- frontend/src/components/markdown/Markdown.tsx
+++ frontend/src/components/markdown/Markdown.tsx
@@ -1,25 +1,38 @@
 /* eslint-disable */
 import React from "react";
-
+import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import markdownStyles from "./Markdown.module.css";
-import { getStrapiURL } from "src/lib/api";
 
 type MarkdownProps = {
   content: string;
 };
 
+const components = {
+  img: ({ ...imgProps }: any) => <img src={imgProps.src} alt={imgProps.alt} />,
+  code({ node, inline, className, children, ...props }: any) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        language={match[1]}
+        style={tomorrow}
+        wrapLines={true}
+        showLineNumbers={true}
+      >
+        {children}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
+
 function Markdown(props: MarkdownProps) {
   return (
-    <ReactMarkdown
-      components={{
-        img: ({ ...imgProps }: any) => (
-          <img src={getStrapiURL(imgProps.src)} alt={imgProps.alt} />
-        ),
-      }}
-      className={markdownStyles.markdown}
-    >
+    <ReactMarkdown components={components} className={markdownStyles.markdown}>
       {props.content}
     </ReactMarkdown>
   );
