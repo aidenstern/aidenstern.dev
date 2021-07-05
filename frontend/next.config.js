481baec0eeacb3e48c132b7ad94816f904ba1cb0
Aiden Stern <aiden.stern@gmail.com>
2021-07-05 13:31:17 -0400

    [add] base configuration for netlify deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const client = require('./src/lib/sanity.server.ts').sanityClient;
+
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
@@ -11,5 +13,21 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
+  },
+  async exportPathMap(defaultPathMap) {
+    const paths = await client
+      .fetch('*[_type == "article" && defined(slug)].slug.current')
+      .then((data) =>
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+            ...acc,
+            [`/article/${slug}`]: { page: '/article/[slug]', query: { slug } },
+          }),
+          defaultPathMap
+        )
+      )
+      .catch(console.error);
+    return paths;
   },
 });
